0fb7611573b57ccde99ca797bb7a6d59b22800b5
Roberto Ramirez <robert.624@hotmail.com>
2023-02-07 10:30:45 -0500

    desktop version done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import { useState } from 'react'
-import divider from './assets/pattern-divider-desktop.svg'
-import diceImg from './assets/icon-dice.svg'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import divider from "./assets/pattern-divider-desktop.svg";
+import diceImg from "./assets/icon-dice.svg";
+import "./App.css";
+
+const BASE_URL = "https://api.adviceslip.com/advice";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [currentAdvice, setCurrentAdvice] = useState({});
 
-  return (
-    <div className="App">
-      <h1 className='advice__number'>ADVICE #117</h1>
-      <div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, molestiae.</p>
-        <img src={divider} alt="divider" />
-        <a href="#" className='new__advice--btn'>
-          <img src={diceImg} alt="dice icon" />
-        </a>
-      </div>
-    </div>
-  )
+    useEffect(() => {
+        getAdvice();
+    }, []);
+
+    async function getAdvice() {
+        const response = await axios.get(BASE_URL);
+
+        setCurrentAdvice(response.data.slip);
+    }
+
+    return (
+        <div className="App">
+            <h1 className="advice__number">ADVICE #{currentAdvice.id}</h1>
+            <div>
+                <p className="advice__text">{`“${currentAdvice.advice}”`}</p>
+                <div className="bottom__images">
+                    <img src={divider} alt="divider" />
+                    <span
+                        onClick={getAdvice}
+                        className="new__advice--btn"
+                    >
+                        <img src={diceImg} alt="dice icon" />
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
